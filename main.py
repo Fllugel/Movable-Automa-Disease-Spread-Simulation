80ad255a72e39bcd3b3fb86d3670319ff539d152
Fllugel <135070466+Fllugel@users.noreply.github.com>
2024-10-21 20:41:57 +0300

    Fixed colors for Mac and Windows

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 from PyQt5.QtWidgets import (QApplication, QMainWindow, QVBoxLayout, QHBoxLayout, QGridLayout,
                              QWidget, QLineEdit, QLabel, QPushButton, QCheckBox, QSpacerItem,
                              QSizePolicy, QScrollArea, QLayout)
+from PyQt5.QtGui import QPalette, QColor
 from game_widget import GameWidget
 
 class MainWindow(QMainWindow):
@@ -108,6 +109,25 @@ class MainWindow(QMainWindow):
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+    app.setStyle("Fusion")
+
+    palette = QPalette()
+    palette.setColor(QPalette.Window, QColor(255, 255, 255))
+    palette.setColor(QPalette.WindowText, QColor(0, 0, 0))
+    palette.setColor(QPalette.Base, QColor(255, 255, 255))
+    palette.setColor(QPalette.AlternateBase, QColor(240, 240, 240))
+    palette.setColor(QPalette.ToolTipBase, QColor(255, 255, 255))
+    palette.setColor(QPalette.ToolTipText, QColor(0, 0, 0))
+    palette.setColor(QPalette.Text, QColor(0, 0, 0))
+    palette.setColor(QPalette.Button, QColor(255, 255, 255))
+    palette.setColor(QPalette.ButtonText, QColor(0, 0, 0))
+    palette.setColor(QPalette.BrightText, QColor(255, 0, 0))
+    palette.setColor(QPalette.Link, QColor(0, 0, 255))
+    palette.setColor(QPalette.Highlight, QColor(0, 120, 215))
+    palette.setColor(QPalette.HighlightedText, QColor(255, 255, 255))
+
+    app.setPalette(palette)
+
     window = MainWindow()
     window.show()
     sys.exit(app.exec_())

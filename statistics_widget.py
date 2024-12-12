28d78a19b215f81c0419725d7740c4cd898d0fb9
Artemidiuss <artemzhykoskuy@gmail.com>
2024-12-12 17:51:42 +0200

    add figure2 in StatisticsWidget

--- statistics_widget.py
+++ statistics_widget.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QScrollArea, QLabel
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QLabel
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 
@@ -6,13 +6,22 @@ class StatisticsWidget(QWidget):
     def __init__(self, parent=None, config=None):
         super().__init__(parent)
         self.config = config
-        self.figure, self.ax = plt.subplots()
-        self.canvas = FigureCanvas(self.figure)
-        self.canvas.setFixedSize(600, 350)
-        self.canvas.setStyleSheet("background-color:white;")
-        self.canvas.setContentsMargins(0, 0, 0, 0)
-        self.figure.patch.set_facecolor('none')
-        self.set_plot_background()
+
+        # Створення першого графіка
+        self.figure1, self.ax1 = plt.subplots()
+        self.canvas1 = FigureCanvas(self.figure1)
+        self.canvas1.setFixedSize(600, 350)
+        self.canvas1.setStyleSheet("background-color:white;")
+        self.figure1.patch.set_facecolor('none')
+        self.set_plot_background(self.ax1)
+
+        # Створення другого графіка
+        self.figure2, self.ax2 = plt.subplots()
+        self.canvas2 = FigureCanvas(self.figure2)
+        self.canvas2.setFixedSize(600, 350)
+        self.canvas2.setStyleSheet("background-color:white;")
+        self.figure2.patch.set_facecolor('none')
+        self.set_plot_background(self.ax2)
 
         self.time_data = []
         self.healthy_data = []
@@ -33,20 +42,22 @@ class StatisticsWidget(QWidget):
 
         layout = QVBoxLayout()
         layout.addLayout(label_layout)
-        layout.addWidget(self.canvas)
+        layout.addWidget(self.canvas1)
+        layout.addWidget(self.canvas2)
         self.setLayout(layout)
 
-    def set_plot_background(self):
-        self.ax.set_facecolor('white')
-        for spine in self.ax.spines.values():
+    def set_plot_background(self, ax):
+        ax.set_facecolor('white')
+        for spine in ax.spines.values():
             spine.set_color('black')
-        self.ax.tick_params(axis='both', colors='black')
-        self.ax.xaxis.label.set_color('black')
-        self.ax.yaxis.label.set_color('black')
+        ax.tick_params(axis='both', colors='black')
+        ax.xaxis.label.set_color('black')
+        ax.yaxis.label.set_color('black')
 
     def update_plot(self):
-        self.ax.clear()
-        self.set_plot_background()
+        # Оновлення першого графіка
+        self.ax1.clear()
+        self.set_plot_background(self.ax1)
 
         total_population = max((h + l + i + d for h, l, i, d in
                                 zip(self.healthy_data, self.latent_data, self.infected_data, self.dead_data)),
@@ -57,20 +68,36 @@ class StatisticsWidget(QWidget):
         latent_infected = [l + i for l, i in zip(self.latent_data, self.infected_data)]
         dead_latent_infected = [l + i + d for l, i, d in zip(self.latent_data, self.infected_data, self.dead_data)]
 
-        self.ax.fill_between(self.time_data, 0, self.latent_data, color=[c/255 for c in self.config.color_latent],
+        self.ax1.fill_between(self.time_data, 0, self.latent_data, color=[c/255 for c in self.config.color_latent],
                              label='Latent')
-        self.ax.fill_between(self.time_data, self.latent_data, latent_infected, color=[c/255 for c in self.config.color_active],
+        self.ax1.fill_between(self.time_data, self.latent_data, latent_infected, color=[c/255 for c in self.config.color_active],
                              label='Infectious')
-        self.ax.fill_between(self.time_data, latent_infected, dead_latent_infected, color=[c/255 for c in self.config.color_dead], label='Dead')
+        self.ax1.fill_between(self.time_data, latent_infected, dead_latent_infected, color=[c/255 for c in self.config.color_dead], label='Dead')
 
         if total_population - max(dead_latent_infected, default=0) > 0:
-            self.ax.fill_between(self.time_data, dead_latent_infected, [total_population] * len(dead_latent_infected),
+            self.ax1.fill_between(self.time_data, dead_latent_infected, [total_population] * len(dead_latent_infected),
                                  color=[c/255 for c in self.config.color_healthy], label='Susceptible')
 
-        legend = self.ax.legend(loc='upper left', facecolor=(37 / 255, 61 / 255, 71 / 255), edgecolor=(1, 1, 1))
-        for text in legend.get_texts():
+        legend1 = self.ax1.legend(loc='upper left', facecolor=(37 / 255, 61 / 255, 71 / 255), edgecolor=(1, 1, 1))
+        for text in legend1.get_texts():
             text.set_color((1, 1, 1))
-        self.canvas.draw()
+        self.canvas1.draw()
+
+        # Оновлення другого графіка
+        self.ax2.clear()
+        self.set_plot_background(self.ax2)
+
+        infectious_latent = [i + l for i, l in zip(self.infected_data, self.latent_data)]
+
+        self.ax2.fill_between(self.time_data, 0, self.infected_data, color=[c/255 for c in self.config.color_active],
+                             label='Infectious')
+        self.ax2.fill_between(self.time_data, self.infected_data, infectious_latent, color=[c/255 for c in self.config.color_latent],
+                             label='Latent')
+
+        legend2 = self.ax2.legend(loc='upper left', facecolor=(37 / 255, 61 / 255, 71 / 255), edgecolor=(1, 1, 1))
+        for text in legend2.get_texts():
+            text.set_color((1, 1, 1))
+        self.canvas2.draw()
 
         self.update_labels()
 
@@ -81,7 +108,8 @@ class StatisticsWidget(QWidget):
         self.dead_label.setText(f"Dead: {self.dead_data[-1] if self.dead_data else 0}")
 
     def save_plot(self):
-        self.figure.savefig('simulation_plot.png', bbox_inches='tight', dpi=300)
+        self.figure1.savefig('simulation_plot1.png', bbox_inches='tight', dpi=300)
+        self.figure2.savefig('simulation_plot2.png', bbox_inches='tight', dpi=300)
 
     def add_data(self, day, healthy, latent, infected, dead):
         self.time_data.append(day)
